6de4f61b85be3eb3874e9da305766e9ca9bf4343
Igor Polyakov <fox.user.3@gmail.com>
2015-12-09 16:25:36 +0600

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt-get update -y -q
+sudo apt-get update -y -qq
 echo "app list is update"
-sudo apt-get install tex-live-full -y -q
+sudo apt-get install texlive-full -y -q
 echo "tex-live install"
 sudo apt-get install scalable-cyrfonts-tex -y -q
 echo "get front from scalable-cyrfonts-tex"
-cd eskdx
-sudo cp *.* /usr/share/texmf/tex/latex
+sudo mkdir -p /usr/share/texmf/tex/latex/eskdx
+sudo cp -r ./eskdx/ /usr/share/texmf/tex/latex
 echo "copy eskdx"
 sudo texhash
 echo "done"
